42c46748a972d44c15744b6c11150d08bfc095bc
jmg292 <24702178+jmg292@users.noreply.github.com>
2022-07-25 16:16:10 -0500

    Refactor: move and rename bit converters

--- internal/utilities/convert/uint_to_bytes.go
+++ internal/utilities/convert/uint_to_bytes.go
@@ -7,3 +7,19 @@ func UInt64ToBytes(value uint64) []byte {
 	}
 	return returnValue
 }
+
+func UInt32ToBytes(value uint32) []byte {
+	returnValue := make([]byte, 4)
+	for i := uint32(0); i < 4; i++ {
+		returnValue[i] = byte((value >> (i * 4)) & 0xFF)
+	}
+	return returnValue
+}
+
+func UInt16ToBytes(value uint16) []byte {
+	returnValue := make([]byte, 2)
+	for i := uint16(0); i < 2; i++ {
+		returnValue[i] = byte((value >> (i * 2)) & 0xFF)
+	}
+	return returnValue
+}
