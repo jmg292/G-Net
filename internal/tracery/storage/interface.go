ead810b7155490b0201bbf30b49b14749e69251c
jmg292 <24702178+jmg292@users.noreply.github.com>
2022-07-27 23:30:19 -0500

    Add BlockStorage interface

--- internal/tracery/storage/interface.go
+++ internal/tracery/storage/interface.go
@@ -1,4 +1,10 @@
 package storage
 
+import "github.com/jmg292/G-Net/pkg/wumbo"
+
 type BlockStorage interface {
+	Open() error
+	Close() error
+	PutBlock(wumbo.Block) error
+	GetBlock([]byte) (wumbo.Block, error)
 }
