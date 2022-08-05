724df69805becf88c42eaa941b02c8356d1cc59b
jmg292 <24702178+jmg292@users.noreply.github.com>
2022-08-04 23:42:14 -0500

    Change != to ==

--- pkg/crypto/private/keystore/file/keyslot/crypto.go
+++ pkg/crypto/private/keystore/file/keyslot/crypto.go
@@ -6,14 +6,14 @@ func (slot *keySlot) Lock(kek []byte) (err error) {
 	if slot.nonceIsEmpty() {
 		slot.generateNonce()
 	}
-	if cipher, err := chacha20poly1305.NewX(kek); err != nil {
+	if cipher, err := chacha20poly1305.NewX(kek); err == nil {
 		cipher.Seal(slot.getKeyWithOverhead(), slot.getNonce(), slot.GetKey(), slot.getKeyTypeBytes())
 	}
 	return
 }
 
 func (slot *keySlot) Unlock(kek []byte) (err error) {
-	if cipher, err := chacha20poly1305.NewX(kek); err != nil {
+	if cipher, err := chacha20poly1305.NewX(kek); err == nil {
 		_, err = cipher.Open(slot.GetKey(), slot.getNonce(), slot.getKeyWithOverhead(), slot.getKeyTypeBytes())
 		return err
 	}
