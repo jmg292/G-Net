ffe326a620ca9f021c492d309e5c2cc5e15b7e13
jmg292 <24702178+jmg292@users.noreply.github.com>
2022-08-04 21:20:21 -0500

    Add KeySlot type

--- pkg/crypto/private/keystore/file/keyslot/keyslot.go
+++ pkg/crypto/private/keystore/file/keyslot/keyslot.go
@@ -1,5 +1,24 @@
 package keyslot
 
+import (
+	"crypto"
+	"fmt"
+
+	gcrypt "github.com/jmg292/G-Net/pkg/crypto"
+	"github.com/jmg292/G-Net/pkg/gnet"
+)
+
 const Size int = 100
 
-type KeySlot [Size]byte
+type keySlot [Size]byte
+
+func Empty() *keySlot {
+	var slot keySlot
+	return &slot
+}
+
+func New(keyType gcrypt.SupportedKeyType, key crypto.PrivateKey, managementKey []byte) (slot *keySlot, err error) {
+	slot = Empty()
+	err = fmt.Errorf(string(gnet.ErrorNotYetImplemented))
+	return
+}
