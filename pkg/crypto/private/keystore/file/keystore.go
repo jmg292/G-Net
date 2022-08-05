0a33004a82f56fd578948a602b4657cf49e77b25
jmg292 <24702178+jmg292@users.noreply.github.com>
2022-08-05 01:23:41 -0500

    Recompose FileKeyStore

--- pkg/crypto/private/keystore/file/keystore.go
+++ pkg/crypto/private/keystore/file/keystore.go
@@ -3,11 +3,27 @@ package file
 import (
 	"fmt"
 
+	"github.com/jmg292/G-Net/pkg/crypto/private/keystore/file/adminslot"
+	"github.com/jmg292/G-Net/pkg/crypto/private/keystore/file/index"
+	"github.com/jmg292/G-Net/pkg/crypto/private/keystore/file/keyslot"
+	"github.com/jmg292/G-Net/pkg/crypto/private/keystore/file/meta"
 	"github.com/jmg292/G-Net/pkg/gnet"
 )
 
 // Exists to facilitate user identity backups
 type fileKeyStore struct {
+	metadata          meta.Meta
+	preamble          []byte
+	versionNumber     byte
+	index             index.Index
+	salt              []byte
+	adminSlot         adminslot.AdminSlot
+	signingKeySlot    keyslot.KeySlot
+	authKeySlot       keyslot.KeySlot
+	encryptionKeySlot keyslot.KeySlot
+	deviceKeySlot     keyslot.KeySlot
+	certificateStore  []byte
+	validationTag     []byte
 }
 
 func New(path string) (*fileKeyStore, error) {
