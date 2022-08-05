715bd86a2e3551cf0cb23ea0148783a599566972
jmg292 <24702178+jmg292@users.noreply.github.com>
2022-08-04 23:12:11 -0500

    Fix key slot indexing

--- pkg/crypto/private/keystore/file/index/key_slots.go
+++ pkg/crypto/private/keystore/file/index/key_slots.go
@@ -5,39 +5,7 @@ import (
 	"github.com/jmg292/G-Net/pkg/crypto/private/keystore/file/keyslot"
 )
 
-func (*index) getSlotOffset(keySlot crypto.KeySlot) int {
+func (*index) GetKeySlotOffset(keySlot crypto.KeySlot) int {
 	relativeOffset := int(keySlot) * keyslot.Size
 	return int(keySlotBase) + relativeOffset
-}
-
-func (i *index) SigningKeySlotOffset() int {
-	return i.getSlotOffset(crypto.SigningKeySlot)
-}
-
-func (*index) SigningKeySlotSize() int {
-	return keyslot.Size
-}
-
-func (i *index) AuthenticationKeySlotOffset() int {
-	return i.getSlotOffset(crypto.AuthenticationKeySlot)
-}
-
-func (*index) AuthenticationKeySlotSize() int {
-	return keyslot.Size
-}
-
-func (i *index) EncryptionKeySlotOffset() int {
-	return i.getSlotOffset(crypto.EncryptionKeySlot)
-}
-
-func (*index) EncryptionKeySlotSize() int {
-	return keyslot.Size
-}
-
-func (i *index) DeviceKeySlotOffset() int {
-	return i.getSlotOffset(crypto.DeviceKeySlot)
-}
-
-func (*index) DeviceKeySlotSize() int {
-	return keyslot.Size
 }
