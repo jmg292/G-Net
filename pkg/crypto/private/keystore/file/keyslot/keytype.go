dbfc9e2fceec3c5d7595ed290c16d924dd5cdc12
jmg292 <24702178+jmg292@users.noreply.github.com>
2022-08-04 21:38:14 -0500

    Add keyslot typing support

---
+++ pkg/crypto/private/keystore/file/keyslot/keytype.go
@@ -0,0 +1,14 @@
+package keyslot
+
+import (
+	"github.com/jmg292/G-Net/internal/utilities/convert"
+	gcrypt "github.com/jmg292/G-Net/pkg/crypto"
+)
+
+func (slot *keySlot) KeyType() gcrypt.SupportedKeyType {
+	return gcrypt.SupportedKeyType(convert.BytesToUInt16(slot[:2]))
+}
+
+func (slot *keySlot) setKeyType(keyType gcrypt.SupportedKeyType) {
+	copy(slot[:2], convert.UInt16ToBytes(uint16(keyType)))
+}
