136d3159b815381a98f56d39761cfaf762e25a6a
jmg292 <24702178+jmg292@users.noreply.github.com>
2022-08-07 17:04:44 -0500

    Add public.KeyRing

--- pkg/keyring/public/keyring.go
+++ pkg/keyring/public/keyring.go
@@ -1,1 +1,18 @@
 package public
+
+import (
+	"crypto"
+
+	"github.com/jmg292/G-Net/pkg/keyring/certificates"
+	"github.com/jmg292/G-Net/pkg/keyring/key"
+)
+
+type KeyRing struct {
+	encryptionKey *key.X25519PublicKey
+	certstore     certificates.CertificateStore
+}
+
+// Implement crypto.PublicKey for public.KeyRing
+func (keyring *KeyRing) Equal(key crypto.PublicKey) bool {
+	return false
+}
