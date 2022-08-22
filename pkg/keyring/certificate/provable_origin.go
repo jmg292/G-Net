11b6d70d5d8012a32c5e10dff566c660f229d839
jmg292 <24702178+jmg292@users.noreply.github.com>
2022-08-22 11:44:04 -0500

    Add interface certificate.ProvableOrigin

---
+++ pkg/keyring/certificate/provable_origin.go
@@ -0,0 +1,24 @@
+package certificate
+
+import (
+	"crypto/x509"
+
+	"github.com/jmg292/G-Net/pkg/gnet"
+	"github.com/jmg292/G-Net/pkg/keyring/certificate/extensions"
+)
+
+type ProvableOrigin interface {
+	Certificate() *x509.Certificate
+	ProofOfOrigin() (*x509.Certificate, error)
+}
+
+func GetProofOfOrigin(certificate Extensible) (proofOfOrigin *x509.Certificate, err error) {
+	if extProofOfOrigin, e := findExtensionByOID(certificate, extensions.OIDProofOfOrigin); e != nil {
+		err = e
+	} else if extProofOfOrigin.Value == nil {
+		err = gnet.ErrorCertificateNotFound
+	} else {
+		proofOfOrigin, err = x509.ParseCertificate(extProofOfOrigin.Value)
+	}
+	return
+}
