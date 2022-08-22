8c519db5ccefbe3a1c782d2b45dd38fa2aa89dc2
jmg292 <24702178+jmg292@users.noreply.github.com>
2022-08-22 13:46:35 -0500

    Set up hardwareBackedCertificate embedding

--- pkg/keyring/certificate/hardware_backed.go
+++ pkg/keyring/certificate/hardware_backed.go
@@ -3,89 +3,48 @@ package certificate
 import (
 	"crypto/x509"
 	"crypto/x509/pkix"
-	"encoding/asn1"
-	"fmt"
-
-	"github.com/jmg292/G-Net/pkg/gnet"
-	"github.com/jmg292/G-Net/pkg/keyring/certificate/extensions"
 )
 
+type HardwareBackedCertificate interface {
+	Extensible
+	ProvableOrigin
+}
+
 type hardwareBackedCertificate struct {
-	Certificate   *x509.Certificate
+	certificate   *x509.Certificate
 	proofOfOrigin *x509.Certificate
-	extensions    map[string]pkix.Extension
+	extensions    *map[string]pkix.Extension
 }
 
-func (h *hardwareBackedCertificate) loadExtensions() (err error) {
-	var unhandledExtensions []pkix.Extension
-	if h.Certificate.Extensions != nil {
-		unhandledExtensions = append(unhandledExtensions, h.Certificate.Extensions...)
-	}
-	if h.Certificate.ExtraExtensions != nil {
-		unhandledExtensions = append(unhandledExtensions, h.Certificate.ExtraExtensions...)
-	}
-	for _, extension := range unhandledExtensions {
-		oid := extension.Id.String()
-		if _, keyExists := h.extensions[oid]; keyExists {
-			err = fmt.Errorf(gnet.ErrorDuplicateExtension.Error(), oid)
-			break
-		} else {
-			h.extensions[oid] = extension
-		}
+// Implement certificate.Extensible and certificate.ProvableOrigin for hardwareBackedCertificate
+func (h hardwareBackedCertificate) Certificate() *x509.Certificate {
+	return h.certificate
+}
+
+// Implement certificate.Extensible for hardwareBackedCertificate
+func (h hardwareBackedCertificate) Extensions() (extensions *map[string]pkix.Extension) {
+	if h.extensions != nil {
+		extensions = h.extensions
 	}
 	return
 }
 
-func (h *hardwareBackedCertificate) loadProofOfOrigin() (err error) {
+// Implement certificate.ProvableOrigin for hardwareBackedCertificate
+func (h hardwareBackedCertificate) ProofOfOrigin() (cert *x509.Certificate, err error) {
 	if h.proofOfOrigin != nil {
-		err = gnet.ErrorCertAlreadyExists
-	} else if attestationCertDer, e := h.GetExtensionByOID(extensions.OIDProofOfOrigin); e != nil {
-		err = e
-	} else if attestationCert, e := x509.ParseCertificate(attestationCertDer.Value); e != nil {
-		err = e
+		cert = h.proofOfOrigin
 	} else {
-		h.proofOfOrigin = attestationCert
+		cert, err = GetProofOfOrigin(h)
 	}
 	return
 }
 
-func (h *hardwareBackedCertificate) ProofOfOrigin() (attestationCert *x509.Certificate, err error) {
-	if h.proofOfOrigin == nil {
-		err = gnet.ErrorInvalidAttestationCert
-	} else {
-		attestationCert = h.proofOfOrigin
-	}
-	return
-}
-
-func (h *hardwareBackedCertificate) GetExtensionByOID(oid asn1.ObjectIdentifier) (extension *pkix.Extension, err error) {
-	if value, exists := h.extensions[oid.String()]; !exists {
-		err = fmt.Errorf(gnet.ErrorNoSuchExtension.Error(), oid.String())
-	} else {
-		extension = &value
-	}
-	return
-}
-
-func (h *hardwareBackedCertificate) LoadCertificate(cert *x509.Certificate) (err error) {
-	if h.Certificate != nil {
-		err = gnet.ErrorCertAlreadyExists
-	} else if cert == nil {
-		err = gnet.ErrorInvalidCertificate
-	} else {
-		h.Certificate = cert
-		if err = h.loadExtensions(); err == nil {
-			err = h.loadProofOfOrigin()
-		}
-	}
-	return
-}
-
-func (h *hardwareBackedCertificate) LoadCertificateDER(der []byte) (err error) {
+func parseHardwareBackedCertificate(der []byte) (hwCert *hardwareBackedCertificate, err error) {
 	if cert, e := x509.ParseCertificate(der); e != nil {
 		err = e
 	} else {
-		h.LoadCertificate(cert)
+		hwCert = &hardwareBackedCertificate{certificate: cert}
+		hwCert.extensions, err = parseExtensions(hwCert)
 	}
 	return
 }
