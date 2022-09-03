54d5d61642c065bc4cd00defe3c86fe355a39d44
jmg292 <24702178+jmg292@users.noreply.github.com>
2022-09-02 20:40:39 -0500

    Update manifest interface

--- internal/tracery/manifest/entry/device.go
+++ internal/tracery/manifest/entry/device.go
@@ -1,4 +1,4 @@
-package entries
+package entry
 
 import (
 	"time"
--- internal/tracery/manifest/interface.go
+++ internal/tracery/manifest/interface.go
@@ -1,4 +1,14 @@
 package manifest
 
+import (
+	"github.com/jmg292/G-Net/internal/datagram/management/edict"
+	"github.com/jmg292/G-Net/internal/tracery/manifest/entry"
+	"github.com/jmg292/G-Net/pkg/identity/certificate"
+)
+
 type Manifest interface {
+	GetIdentity([]byte) (*certificate.Identity, error)
+	GetNetworkExits([]byte) ([]*edict.NetworkExit, error)
+	GetNetworkRoutes([]byte) ([]*edict.NetworkRoute, error)
+	GetDeviceManifest([]byte) (*entry.DeviceManifest, error)
 }
