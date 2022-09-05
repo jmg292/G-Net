ba7198d776bc4a0ea174e6bcfb04e8ffa506d97e
jmg292 <24702178+jmg292@users.noreply.github.com>
2022-09-05 13:37:44 -0500

    Add internal logging skeleton

--- internal/host/interface.go
+++ internal/host/interface.go
@@ -1,1 +1,24 @@
 package host
+
+import gnet "github.com/jmg292/G-Net/pkg/gneterrs"
+
+type LogLevel uint8
+
+const (
+	Unknown LogLevel = iota
+	Trace
+	Debug
+	Info
+	Warn
+	Error
+	Fatal
+	Panic
+)
+
+func GetHost() (Host, error) {
+	return nil, gnet.ErrorNotYetImplemented
+}
+
+type Host interface {
+	Log(string, LogLevel)
+}
